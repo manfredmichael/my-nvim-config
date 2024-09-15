b22cc6b5ca58c8fe31d514e3507a9403c58c247d
manfredmichael <koskosanrider@gmail.com>
2024-09-15 21:53:12 +0700

    Update oil.nvim keybind

--- lua/configs/oil.lua
+++ lua/configs/oil.lua
@@ -63,8 +63,8 @@ M.setup = function()
     keymaps = {
       ["g?"] = "actions.show_help",
       ["<CR>"] = "actions.select",
-      ["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
-      ["<C-h>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
+      ["<C-v>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+      ["<C-s>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
       ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
       ["<C-p>"] = "actions.preview",
       ["<C-c>"] = "actions.close",
--- lua/mappings/oil.lua
+++ lua/mappings/oil.lua
@@ -1,3 +1,5 @@
 vim.keymap.set("n", "<C-c>", ":Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "<C-s>", ":split<CR>", { desc = "Split horizontally" })
+vim.keymap.set("n", "<C-v>", ":vsplit<CR>", { desc = "Split vertically" })
 
 
